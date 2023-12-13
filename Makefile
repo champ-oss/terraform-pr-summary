f5e1ecb523cb07e625804741066ef1344bc2f9ad
Justin Reeves <reeves122@gmail.com>
2023-12-13 11:25:05 -0600

    add doctrings. add test workflow

---
+++ Makefile
@@ -0,0 +1,18 @@
+coverage:
+	pip3 install coverage
+	coverage run -m pytest --ignore=glue
+	coverage html --omit="test_*.py"
+	coverage xml --omit="test_*.py"
+	open htmlcov/index.html || true
+
+check-coverage:
+	coverage run -m pytest --ignore=glue
+	coverage xml --omit="test_*.py"
+	coverage report --fail-under=80
+
+lint:
+	pip3 install flake8
+	flake8 --exclude=venv/,terraform/ --max-line-length=130 .
+
+test:
+	coverage run -m pytest --log-cli-level=INFO
--- main.py
+++ main.py
@@ -6,12 +6,24 @@ from typing import List
 
 
 def get_terraform_plan_json(plan_file_path: str) -> str:
+    """
+    Get the Terraform plan as a JSON string
+
+    :param plan_file_path: local path for Terraform plan file
+    :return: Terraform plan as JSON string
+    """
     command = ['terraform', 'show', '-json', plan_file_path]
     print('running command: ' + ' '.join(command))
     return subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8')
 
 
 def parse_resource_changes(plan_json: str) -> List[dict]:
+    """
+    Parse the JSON Terraform Plan and return the list of resource changes
+
+    :param plan_json: Terraform plan as JSON string
+    :return: list of Terraform resource changes
+    """
     try:
         plan = json.loads(plan_json)
         return plan.get('resource_changes', [])
@@ -20,29 +32,64 @@ def parse_resource_changes(plan_json: str) -> List[dict]:
         raise Exception
 
 
-# https://developer.hashicorp.com/terraform/internals/json-format#change-representation
 def get_create_changes(resource_changes: List[dict]) -> List[str]:
+    """
+    Parse the Terraform plan resource changes and return a list of resource being created
+    https://developer.hashicorp.com/terraform/internals/json-format#change-representation
+
+    :param resource_changes: list of Terraform resource changes
+    :return: list of Terraform resources being created
+    """
     return [resource['address'] for resource in resource_changes
             if resource['change']['actions'] == ['create']]
 
 
 def get_update_changes(resource_changes: List[dict]) -> List[str]:
+    """
+    Parse the Terraform plan resource changes and return a list of resource being updated
+    https://developer.hashicorp.com/terraform/internals/json-format#change-representation
+
+    :param resource_changes: list of Terraform resource changes
+    :return: list of Terraform resources being updated
+    """
     return [resource['address'] for resource in resource_changes
             if resource['change']['actions'] == ['update']]
 
 
 def get_delete_changes(resource_changes: List[dict]) -> List[str]:
+    """
+    Parse the Terraform plan resource changes and return a list of resource being deleted
+    https://developer.hashicorp.com/terraform/internals/json-format#change-representation
+
+    :param resource_changes: list of Terraform resource changes
+    :return: list of Terraform resources being deleted
+    """
     return [resource['address'] for resource in resource_changes
             if resource['change']['actions'] == ['delete']]
 
 
 def get_replace_changes(resource_changes: List[dict]) -> List[str]:
+    """
+    Parse the Terraform plan resource changes and return a list of resource being replaced
+    https://developer.hashicorp.com/terraform/internals/json-format#change-representation
+
+    :param resource_changes: list of Terraform resource changes
+    :return: list of Terraform resources being replaced
+    """
     return [resource['address'] for resource in resource_changes
             if resource['change']['actions'] == ["delete", "create"] or
             resource['change']['actions'] == ["create", "delete"]]
 
 
-def append_to_output(output: str, items: List[str], description: str):
+def append_to_output(output: str, items: List[str], description: str) -> str:
+    """
+    Append a list of Terraform resources and a description to the output string
+
+    :param output: overall terraform plan output string
+    :param items: list of Terraform resources
+    :param description: description of resources (ex: created, deleted, etc)
+    :return: appended output string
+    """
     if not items:
         return output
     output += f'\n**{description}**:\n'
@@ -52,13 +99,26 @@ def append_to_output(output: str, items: List[str], description: str):
 
 
 def build_output_header(custom_identifier: str) -> str:
+    """
+    Set a header for the terraform plan output string
+
+    :param custom_identifier: custom string to add to the header (ex: tfvars filename)
+    :return: appended output string
+    """
     if custom_identifier:
         print(f'using custom identifier: {custom_identifier}')
         return f'**Terraform Plan ({custom_identifier})**\n\n'
-    return f'**Terraform Plan**\n\n'
+    return '**Terraform Plan**\n\n'
 
 
 def append_changes_to_output(output: str, resource_changes: List[dict]) -> str:
+    """
+    Get the list of created, updated, replaced, deleted resources and append to the output summary
+
+    :param output: overall terraform plan output string
+    :param resource_changes: list of Terraform resource changes
+    :return: appended output string
+    """
     create = get_create_changes(resource_changes)
     update = get_update_changes(resource_changes)
     replace = get_replace_changes(resource_changes)
@@ -75,12 +135,25 @@ def append_changes_to_output(output: str, resource_changes: List[dict]) -> str:
 
 
 def write_output(output: str, file_name: str) -> None:
+    """
+    Write the output string to the given file name
+
+    :param output: overall terraform plan output string
+    :param file_name: local file name to write
+    :return: None
+    """
     print(f'writing output to file: {file_name}')
     with open(file_name, 'w') as f:
         f.write(output)
 
 
-def main():
+def main() -> None:
+    """
+    Main entrypoint for this script. This will parse the Terraform plan file as JSON, get a list of resource
+    changes, and create a summary string that can be added as a comment to a pull request.
+
+    :return: None
+    """
     custom_identifier = os.getenv('IDENTIFIER')
     plan_file = os.getenv('PLAN_FILE')
     output_file = os.getenv('OUTPUT_FILE')
